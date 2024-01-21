53cfd64b47ba1bcb9c5504bb6dca8ccb76e6934c
RunSoo <109501116+RunSoo@users.noreply.github.com>
2024-01-21 20:15:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,16 +1,48 @@
 function solution(n) {
     var answer = 0;
     
-    function oneCnt(number) {
-        return number.toString(2).split("1").length-1;
-    }
+    let nStr = n.toString(2);
+    // console.log(nStr);
     
-    answer=n+1;
+    let ansStr = new Array(nStr.length+1).fill('0');
     
-    while (true){
-        if (oneCnt(answer)===oneCnt(n)){
-            return answer;
+    let change = -1;
+    let cnt1 = 0;
+    
+    for (let i=0; i<nStr.length; i++){
+        if (nStr[i]==='1'){
+            cnt1++;
+        } else if (nStr[i]==='0' && i<nStr.length-1 && nStr[i+1]==='1'){
+            change=i;
+            break;
         }
-        answer++;
     }
+    // console.log('change', change);
+    
+    
+    if (change!==-1){
+        ansStr[change+1]='1';
+        for (let i=1; i<change; i++){
+        ansStr[i]=nStr[i-1]+'';
+    }
+    for (let i=0; i<cnt1+1; i++){
+        ansStr[ansStr.length-1-i]='1';
+    }
+    } else {
+        ansStr[0]='1';
+        for (let i=0; i<cnt1-1; i++){
+            ansStr[ansStr.length-1-i]='1';
+        }
+    }
+    
+    // console.log(ansStr);
+    
+    let next = '';
+    for (let i=0; i<ansStr.length; i++){
+        next+=ansStr[i];
+    }
+    
+    answer = parseInt(next, 2);
+    
+    return answer;
 }
