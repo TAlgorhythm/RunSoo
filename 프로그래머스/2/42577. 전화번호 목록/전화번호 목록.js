9714a935c042b0ef29dff76985dd1b14e45b7962
RunSoo <109501116+RunSoo@users.noreply.github.com>
2024-03-17 21:47:58 +0900

    [level 2] Title: 전화번호 목록, Time: 105.09 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,11 @@
+function solution(phoneBook) {
+    phoneBook.sort();
+
+    for (let i = 0; i < phoneBook.length - 1; i++) {
+        if (phoneBook[i + 1].startsWith(phoneBook[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
